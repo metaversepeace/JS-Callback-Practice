f8b407091be381b34c69557549d244b689b8858b
James Broucek <jbroucek@Jamess-iMac.local>
2021-11-15 16:50:12 -0800

    done but no bonus yet

--- move.js
+++ move.js
@@ -1,12 +1,89 @@
 function move(element) {
-    element.style.position = 'fixed'
+
+    element.style.position = 'fixed';
 
     function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
+
+        element.style.left = left + 'px';
+
         element.style.bottom = bottom + 'px'
-    }
+
+    };
+
+    function moveWithArrowKeys(left, bottom, callback){
+
+        let direction = null;
+
+        let x = 100;
+
+        let y = 250;
+
+        element.style.left = x + 'px';
+
+        element.style.bottom = y + 'px';
+        
+        function moveCharacter() {
+
+            if(direction === 'west'){
+            x -= 1
+            };
+
+            if(direction === 'north'){
+                y += 1
+            };
+
+            if(direction === 'east'){
+                x += 1
+            };
+
+            if(direction === 'south'){
+                y -= 1
+            };
+        
+            element.style.left = x + 'px';
+
+            element.style.bottom = y + 'px'
+        
+        };
+        
+        setInterval(moveCharacter, 1);                                  
+        
+        move(character).to(100, 250);
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            };
+
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            };
+
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            };
+
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            };
+
+            callback(direction)
+        });
+        
+        document.addEventListener('keyup', function(e){
+
+            direction = null;
+
+            callback(direction);
+
+        })
+    };
 
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
+
